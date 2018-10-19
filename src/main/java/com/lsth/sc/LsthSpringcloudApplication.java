102370c18064960b2d0b22ee2382570690c5948f
Kamisamo <Kamisamo@users.noreply.github.com>
2018-10-19 17:30:11 +0800

    init

---
+++ src/main/java/com/lsth/sc/LsthSpringcloudApplication.java
@@ -0,0 +1,12 @@
+package com.lsth.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsthSpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsthSpringcloudApplication.class, args);
+	}
+}
